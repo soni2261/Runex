511948d926a265bc4ad0c7173ff80476606665d9
soni2261 <soniarahal12@gmail.com>
2021-03-18 14:14:31 -0400

    Update objectifs.dart

--- runex/lib/Screens/Profil/Onglets/Objectifs/objectifs.dart
+++ runex/lib/Screens/Profil/Onglets/Objectifs/objectifs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:runex/Screens/Profil/Onglets/Objectifs/Creation_Objectifs/CreerObjectif.dart';
+import 'package:runex/constants.dart';
 
 class Objectifs extends StatefulWidget {
   Objectifs({Key key}) : super(key: key);
@@ -11,6 +12,7 @@ class Objectifs extends StatefulWidget {
 class _ObjectifsState extends State<Objectifs> {
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return Center(
       child: ElevatedButton(
         onPressed: () {
@@ -19,7 +21,31 @@ class _ObjectifsState extends State<Objectifs> {
           ));
         },
         child: Text('Créer un objectif'),
+=======
+    return Column(children: [
+      Align(
+        alignment: Alignment.centerRight,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Ink(
+            decoration: const ShapeDecoration(
+              color: kPrimaryColor,
+              shape: CircleBorder(),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.mode_outlined),
+              color: kPrimaryLightColor,
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  //builder: (context) => CreerObjectif(),
+                  builder: (context) => CreerObjectif(),
+                ));
+              },
+            ),
+          ),
+        ),
+>>>>>>> parent of 8a64055 (Revert "Merge branch 'main' of https://github.com/soni2261/Runex into main")
       ),
-    );
+    ]);
   }
 }
