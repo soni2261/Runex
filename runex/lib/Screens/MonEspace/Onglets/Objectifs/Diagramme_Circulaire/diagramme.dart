ff293ccb14eeebe2e18248ab3679f92b55fe31c2
Ali Taladar <taladarali@gmail.com>
2021-04-04 19:44:23 -0400

    objectifs 1.0

---
+++ runex/lib/Screens/MonEspace/Onglets/Objectifs/Diagramme_Circulaire/diagramme.dart
@@ -0,0 +1,233 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/gestures.dart';
+import 'package:provider/provider.dart';
+import 'package:runex/components/loading.dart';
+import 'package:runex/models/user.dart';
+import 'package:runex/services/database.dart';
+
+class Diagramme extends StatefulWidget {
+  final String typeObjectif;
+
+  const Diagramme({Key key, this.typeObjectif});
+
+  @override
+  State<StatefulWidget> createState() => DiagrammeState();
+}
+
+class DiagrammeState extends State<Diagramme> {
+  int touchedIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    Utilisateur utilisateur = Provider.of<Utilisateur>(context);
+
+    return StreamBuilder<Utilisateur>(
+      stream: DatabaseService(uid: utilisateur.uid).userData,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          utilisateur = snapshot.data;
+
+          if (widget.typeObjectif == "objTemps" &&
+              !utilisateur.objectifs['hasobjTemps']) {
+            return Text(
+              "Pas d'objectif de temps choisi!",
+              style: TextStyle(fontSize: 22, color: Colors.grey),
+            );
+          } else if (widget.typeObjectif == "objDistance" &&
+              !utilisateur.objectifs['hasobjDistance']) {
+            return Text(
+              "Pas d'objectif de distance choisi!",
+              style: TextStyle(fontSize: 22, color: Colors.grey),
+            );
+          }
+
+          Map data = getData(widget.typeObjectif, utilisateur);
+
+          if (widget.typeObjectif == "objTemps" && data.isEmpty) {
+            return Text(
+              "Pas d'objectif de temps choisi!",
+              style: TextStyle(fontSize: 22, color: Colors.grey),
+            );
+          } else if (widget.typeObjectif == "objDistance" && data.isEmpty) {
+            return Text(
+              "Pas d'objectif de distance choisi!",
+              style: TextStyle(fontSize: 22, color: Colors.grey),
+            );
+          }
+
+          return AspectRatio(
+            aspectRatio: 3 / 2,
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: PieChart(
+                PieChartData(
+                  pieTouchData: PieTouchData(touchCallback: (pieTouchResponse) {
+                    setState(() {
+                      final desiredTouch =
+                          pieTouchResponse.touchInput is! PointerExitEvent &&
+                              pieTouchResponse.touchInput is! PointerUpEvent;
+                      if (desiredTouch &&
+                          pieTouchResponse.touchedSection != null) {
+                        touchedIndex = pieTouchResponse.touchedSectionIndex;
+                      } else {
+                        touchedIndex = -1;
+                      }
+                    });
+                  }),
+                  borderData: FlBorderData(
+                    show: false,
+                  ),
+                  sectionsSpace: 0,
+                  centerSpaceRadius: 0,
+                  sections: showingSections(data),
+                ),
+              ),
+            ),
+          );
+        } else {
+          return Loading();
+        }
+      },
+    );
+  }
+
+  List<PieChartSectionData> showingSections(Map data) {
+    int dataLength = data.length;
+    List keyList = data.keys.toList();
+
+    return List.generate(dataLength, (i) {
+      final isTouched = i == touchedIndex;
+      final double fontSize = isTouched ? 20 : 16;
+      final double radius = isTouched ? 110 : 100;
+      final double widgetSize = isTouched ? 55 : 40;
+      if (keyList.contains("marche")) {
+        keyList.remove("marche");
+        String title = widget.typeObjectif == "objTemps"
+            ? "${data["marche"] ~/ 60}H ${data["marche"] % 60}M"
+            : "${data["marche"]} KM";
+        return PieChartSectionData(
+          color: const Color.fromRGBO(255, 90, 90, 1),
+          value: data["marche"].toDouble(),
+          title: title,
+          radius: radius,
+          titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff)),
+          badgeWidget: _Badge(
+            'assets/icons/walk_chart_icon.svg',
+            size: widgetSize,
+            borderColor: const Color(0xfff8b250),
+          ),
+          badgePositionPercentageOffset: 1.1,
+        );
+      } else if (keyList.contains("velo")) {
+        keyList.remove("velo");
+        String title = widget.typeObjectif == "objTemps"
+            ? "${data["velo"] ~/ 60}H ${data["velo"] % 60}M"
+            : "${data["velo"]} KM";
+        return PieChartSectionData(
+          color: const Color.fromRGBO(19, 211, 142, 1),
+          value: data["velo"].toDouble(),
+          title: title,
+          radius: radius,
+          titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff)),
+          badgeWidget: _Badge(
+            'assets/icons/bike_chart_icon.svg',
+            size: widgetSize,
+            borderColor: const Color(0xff13d38e),
+          ),
+          badgePositionPercentageOffset: 1.1,
+        );
+      } else if (keyList.contains("course")) {
+        keyList.remove("course");
+        String title = widget.typeObjectif == "objTemps"
+            ? "${data["course"] ~/ 60}H ${data["course"] % 60}M"
+            : "${data["course"]} KM";
+        return PieChartSectionData(
+          color: const Color.fromRGBO(87, 163, 184, 1),
+          value: data["course"].toDouble(),
+          title: title,
+          radius: radius,
+          titleStyle: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffffffff)),
+          badgeWidget: _Badge(
+            'assets/icons/running_chart_icon.svg',
+            size: widgetSize,
+            borderColor: const Color(0xff845bef),
+          ),
+          badgePositionPercentageOffset: 1.1,
+        );
+      }
+    });
+  }
+
+  Map getData(String typeObjectif, Utilisateur utilisateur) {
+    Map<String, int> data = Map();
+    if (utilisateur.objectifs[typeObjectif]['velo'] != 0) {
+      data['velo'] = utilisateur.objectifs[typeObjectif]['velo'];
+    }
+    if (utilisateur.objectifs[typeObjectif]['marche'] != 0) {
+      data['marche'] = utilisateur.objectifs[typeObjectif]['marche'];
+    }
+    if (utilisateur.objectifs[typeObjectif]['course'] != 0) {
+      data['course'] = utilisateur.objectifs[typeObjectif]['course'];
+    }
+
+    if (data.isNotEmpty)
+      return data;
+    else
+      return null;
+  }
+}
+
+class _Badge extends StatelessWidget {
+  final String svgAsset;
+  final double size;
+  final Color borderColor;
+
+  const _Badge(
+    this.svgAsset, {
+    Key key,
+    @required this.size,
+    @required this.borderColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: PieChart.defaultDuration,
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: borderColor,
+          width: 0,
+        ),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black.withOpacity(.5),
+            offset: const Offset(3, 3),
+            blurRadius: 3,
+          ),
+        ],
+      ),
+      child: Center(
+        child: SvgPicture.asset(
+          svgAsset,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
--- runex/lib/Screens/MonEspace/Onglets/Objectifs/objectifs.dart
+++ runex/lib/Screens/MonEspace/Onglets/Objectifs/objectifs.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:runex/components/theme.dart';
 import 'package:runex/constants.dart';
 import 'Creation_Objectifs/creer_objectif.dart';
+import 'Diagramme_Circulaire/diagramme.dart';
 
 class Objectifs extends StatefulWidget {
   Objectifs({Key key}) : super(key: key);
@@ -15,31 +16,67 @@ class _ObjectifsState extends State<Objectifs> {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeChanger>(context);
-    return Column(children: [
-      Align(
-        alignment: Alignment.centerRight,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Ink(
-            decoration: ShapeDecoration(
-              color: theme.isDark()
-                  ? Color.fromRGBO(40, 35, 55, 1)
-                  : kPrimaryColor,
-              shape: CircleBorder(),
-            ),
-            child: IconButton(
-              icon: Icon(Icons.mode_outlined),
-              color: kPrimaryLightColor,
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => CreerObjectif(),
-                  //builder: (context) => CreerObjectif(),
-                ));
-              },
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Ink(
+                decoration: ShapeDecoration(
+                  color: theme.isDark()
+                      ? Color.fromRGBO(40, 35, 55, 1)
+                      : kPrimaryColor,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.mode_outlined),
+                  color: kPrimaryLightColor,
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => CreerObjectif(),
+                    ));
+                  },
+                ),
+              ),
             ),
           ),
-        ),
+          Text(
+            "Objectifs de distance",
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Diagramme(
+            typeObjectif: "objDistance",
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Divider(
+              color: theme.isDark() ? Colors.grey[200] : Colors.grey,
+              thickness: 1.5,
+            ),
+          ),
+          SizedBox(
+            height: 18,
+          ),
+          Text(
+            "Objectifs de temps",
+            style: TextStyle(fontSize: 24),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Diagramme(
+            typeObjectif: "objTemps",
+          )
+        ],
       ),
-    ]);
+    );
   }
 }
