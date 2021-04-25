fa453561229758ef03fa5551f0f013e84da710d9
soni2261 <soniarahal12@gmail.com>
2021-04-25 01:27:44 -0400

    autocomplete

---
+++ runex/lib/Screens/Carte/components/address_search.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:runex/Screens/Carte/components/place_service.dart';
+
+class AddressSearch extends SearchDelegate<Suggestion> {
+  final sessionToken;
+  PlaceApiProvider apiClient;
+
+  AddressSearch(this.sessionToken) {
+    apiClient = PlaceApiProvider(sessionToken);
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.arrow_back,
+      ),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+        future: query == "" ? null : apiClient.fetchSuggestions(query),
+        builder: (context, snapshot) => query == ""
+            ? Container(
+                child: Text("Entrez votre addresse"),
+              )
+            : snapshot.hasData
+                ? ListView.builder(
+                    itemBuilder: (context, i) => ListTile(
+                      title: Text((snapshot.data[i] as Suggestion).description),
+                      onTap: () {},
+                    ),
+                    itemCount: snapshot.data.length,
+                  )
+                : Container(
+                    child: Center(child: CircularProgressIndicator()),
+                  ));
+  }
+}
